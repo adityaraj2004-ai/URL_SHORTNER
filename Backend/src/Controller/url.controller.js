e2a5d3f7fcd752ed0fa6c0a7d108cfec13510bdf
Aditya Raj <aditya.raj781881@gmail.com>
2026-05-25 23:25:34 +0530

    get Url details controller and get url analytics controller

--- Backend/src/Controller/url.controller.js
+++ Backend/src/Controller/url.controller.js
@@ -1,5 +1,6 @@
 import { Url } from "../Models/Url.js";
 import ApiError from "../Utils/ApiError.js";
+import ApiResponse from "../Utils/ApiResponse.js"
 import { asyncHandler } from "../Utils/asyncHandler.js";
 import { nanoid } from "nanoid"
 
@@ -55,3 +56,35 @@ export const redirectUrl = asyncHandler(async (req, res) => {
 
     return res.redirectUrl(url.originalUrl);
 })
+
+
+export const getUrlDetails = asyncHandler(async (req, res) => {
+    const urlId = req.params;
+    const url = await Url.findOne({
+        urlId
+    })
+    if (!url) {
+        throw new ApiError(404, "No Url Found")
+    }
+    return res.status(200).json(new ApiResponse(200, url, "URL details fetched"))
+
+})
+
+
+// GET /api/v1/url/analytics/:shortId
+export const getUrlAnalytics = asyncHandler(async (req, res) => {
+    const urlId = req.params;
+    const url = await Url.findOne({
+        urlId
+    })
+    return res.status(200).json(new ApiError(200,
+        {
+            totalClicks: url.clicks,
+            visitHistory: url.visitHistory,
+
+        }, "Analytics Fetched"
+
+    ))
+})
+
+// DELETE /api/v1/url/:shortId
