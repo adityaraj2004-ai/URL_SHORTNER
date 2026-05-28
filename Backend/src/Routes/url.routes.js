22f9869776c07f2d23035a75d4cec14df579801e
Aditya Raj <aditya.raj781881@gmail.com>
2026-05-28 23:23:39 +0530

    Added all the routes

--- Backend/src/Routes/url.routes.js
+++ Backend/src/Routes/url.routes.js
@@ -1,9 +1,77 @@
-import { Router } from "express";
-import { shortenUrl } from "../Controller/url.controller.js";
+import express from "express";
 
-const urlRouter = Router();
+import {
+    shortenUrl,
+    getAllUrls,
+    getUrlDetails,
+    updateUrl,
+    deleteUrl,
+    deactivateUrl,
+    activateUrl,
+    getAnalytics,
+    generateQRCode
+} from "../controllers/url.controller.js";
 
-urlRouter.get("/shorten", shortenUrl)
+import {
+    generalLimiter,
+    createUrlLimiter
+} from "../middlewares/rateLimit.middleware.js";
 
+const router = express.Router();
 
-export default urlRouter;
+router.post(
+    "/shorten",
+    createUrlLimiter,
+    shortenUrl
+);
+
+router.get(
+    "/all",
+    generalLimiter,
+    getAllUrls
+);
+
+router.get(
+    "/analytics/:shortId",
+    generalLimiter,
+    getAnalytics
+);
+
+router.get(
+    "/qr/:shortId",
+    generalLimiter,
+    generateQRCode
+);
+
+router.get(
+    "/:shortId",
+    generalLimiter,
+    getUrlDetails
+);
+
+router.patch(
+    "/update/:shortId",
+    generalLimiter,
+    updateUrl
+);
+
+router.patch(
+    "/deactivate/:shortId",
+    generalLimiter,
+    deactivateUrl
+);
+
+router.patch(
+    "/activate/:shortId",
+    generalLimiter,
+    activateUrl
+);
+
+router.delete(
+    "/:shortId",
+    generalLimiter,
+    deleteUrl
+);
+
+export default router;
+
